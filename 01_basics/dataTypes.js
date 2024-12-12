b0ea7f94bdf6712ad5d1f0b4ec7064cd374fa274
Rp <153364976+RishurajPandey@users.noreply.github.com>
2024-12-12 10:53:55 +0530

    dataTypes

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,20 @@
+"use strict";
+
+// alert(3+3) //we are using node.js not browser.
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+
+// number => 2^53
+// bigInt (especially in tradin,stock market, etc)
+// string => " "
+//boolean
+//null =? standalone value
+// undefined=> 
+//symbol=> unique
+
+
+//object
+
+console.log(typeof undefined);
